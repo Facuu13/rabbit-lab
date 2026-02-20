61b43df82601f44b0cafcd7989b4e1d0490c5d0f
Facuu13 <facundoo30@gmail.com>
2026-02-20 19:22:19 -0300

    Dead Letter Queues

--- Iot/worker_db.py
+++ Iot/worker_db.py
@@ -1,29 +1,44 @@
 import pika
 import json
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters('localhost')
-)
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
 channel.exchange_declare(exchange='iot_exchange', exchange_type='topic')
 
-result = channel.queue_declare(queue='telemetry_db', durable=True)
-channel.queue_bind(
-    exchange='iot_exchange',
+# DLQ (cola para mensajes fallidos)
+channel.queue_declare(queue='telemetry_db_dlq', durable=True)
+
+# Cola principal con DLQ configurada
+channel.queue_declare(
     queue='telemetry_db',
-    routing_key='telemetry.*'
+    durable=True,
+    arguments={
+        'x-dead-letter-exchange': '',
+        'x-dead-letter-routing-key': 'telemetry_db_dlq'
+    }
 )
+
+# Recibe toda telemetría
+channel.queue_bind(exchange='iot_exchange', queue='telemetry_db', routing_key='telemetry.*')
 
 def callback(ch, method, properties, body):
-    data = json.loads(body)
-    print("Guardando en DB:", data)
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+    try:
+        data = json.loads(body.decode())
 
-channel.basic_consume(
-    queue='telemetry_db',
-    on_message_callback=callback
-)
+        # Simular falla
+        if data.get("temp", 0) > 30:
+            raise RuntimeError("DB caída simulada")
+
+        print("Guardado OK:", data)
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    except Exception as e:
+        print("Error procesando:", e)
+        # Rechazamos y mandamos a DLQ
+        ch.basic_nack(delivery_tag=method.delivery_tag, requeue=False)
+
+channel.basic_consume(queue='telemetry_db', on_message_callback=callback)
 
 print("Worker DB esperando...")
 channel.start_consuming()
